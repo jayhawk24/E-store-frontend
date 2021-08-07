ac91cd6930e6290bc20439329f3a0828b36bab28
Anubhav Gupta <anubhavthrilling24@gmail.com>
2021-08-07 16:28:46 +0530

    Add product card

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-function ProductCard() {
+function ProductCard(props) {
+    const { id, title, price, images } = props.product;
     return (
         <div class="product-card">
             <div class="icons">
                 <i class="fa fa-heart" aria-hidden="true"></i>
                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>
             </div>
-            <a href="/product/{prod.id}">
-                <img src="{ prod.images[0] }" alt="" />
-                <div class="name">{prod.title}</div>
-                <div class="price">&#8377;{prod.price}</div>
+            <a href={`/product/${id}`}>
+                <img src={images[0]} alt="" />
+                <div class="name">{title}</div>
+                <div class="price">&#8377;{price}</div>
             </a>
         </div>
     );
--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,18 @@
 import { useEffect } from 'react';
 import Layout from '../components/Layout';
 import styles from '../styles/Products.module.css';
+import ProductCard from '../components/ProductCard';
 
 export default function Products() {
     const [products, setProducts] = useState([]);
-    useEffect(() => {});
+    useEffect(() => {
+        const getProducts = async () => {
+            const response = await fetch('products');
+            const data = await response.json();
+            setProducts(data);
+        };
+        getProducts();
+    }, []);
     return (
         <Layout>
             <link rel="stylesheet" href="/css/app.css" />
@@ -12,7 +20,11 @@ export default function Products() {
             <div class="products">
                 <h2>Newest Products</h2>
 
-                <div class="cards-container"></div>
+                <div class="cards-container">
+                    {products.map((product) => {
+                        <ProductCard product={product} />;
+                    })}
+                </div>
             </div>
         </Layout>
     );
